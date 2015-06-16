638af42eb185e195a4e2539845f3962516813f04
Robert Stanton <rstanton@illinois.edu>
2015-06-16 00:06:49 -0500

    start for solution 3 in ruby

--- solutions.rb
+++ solutions.rb
@@ -28,7 +28,20 @@ while curr < 4000000
     temp = prev
     prev = curr
     curr = temp + curr
-    puts curr
+    #puts curr
 end
 
 puts "answer for problem 2 is: " + sum.to_s
+
+##############
+# Problem 3
+##############
+# largest prime factor of 600851475143
+array = []
+for i in 1...600851475143
+    if (600851475143 % i == 0)
+    #if (839 % i == 0)
+        array.push(i)
+    end
+end
+puts array
